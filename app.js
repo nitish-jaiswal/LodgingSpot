d81e506ab607202cd4127047a76ada6f949b9236
Nitish Jaiswal <nitishjaiswal2708@gmail.com>
2024-09-30 18:40:48 +0530

    lodging spot

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production"){
+if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
@@ -37,15 +37,15 @@ async function main() {
     await mongoose.connect(dbUrl);
 }
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate); 
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: MONGO_URL,
     crypto: {
         secret: process.env.SECRET,
     },
@@ -56,15 +56,15 @@ store.on("error", () => {
     console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
-const sessionOptions={
+const sessionOptions = {
     store: store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie:{
+    cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly :true,
+        httpOnly: true,
     }
 }
 
@@ -81,7 +81,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
@@ -93,7 +93,7 @@ app.use((req,res,next)=>{
 //         email: "student@gmail.com",
 //         username: 'delta-student',
 //     });
-    
+
 //     let registeredUser= await User.register(fakeUser,"helloworld");
 //     res.send(registeredUser);
 // });
@@ -119,13 +119,13 @@ app.use("/", userRouter);
 // });
 
 
-app.all("*", (req,res,next) => {
-    next(new ExpressError(404,"Page Not Found!"));
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
 })
 
-app.use( (err,req,res,next) => {
-    let {statusCode = 404 ,message = "Page Not Found!"} = err;
-    res.status(statusCode).render("error.ejs", {message});
+app.use((err, req, res, next) => {
+    let { statusCode = 404, message = "Page Not Found!" } = err;
+    res.status(statusCode).render("error.ejs", { message });
     // res.status(statusCode).send(message);
 })
 
